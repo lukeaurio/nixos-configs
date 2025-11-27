6f0fec75b0add229ea7046bdc82c9928ed4c4e19
Lukas Aurio <lukeaurio@proton.me>
2025-11-27 10:51:58 -0500

    feat: add to nvf config

--- nixos/etc/nixos/programming-configuration.nix
+++ nixos/etc/nixos/programming-configuration.nix
@@ -53,59 +53,106 @@ in {
   programs.ssh.startAgent = true;
 
   programs.nvf = {
-    enable = true;  
+    enable = true;
     # Your settings need to go into the settings attribute set
     # most settings are documented in the appendix
     settings = {
-       vim = {
-         languages = { 
-             nix.enable = true;
-             python.enable = true;
-             rust.enable = true;
-             go.enable = true;
-             #enableLSP = true;
-             enableTreesitter = true;
+      vim = {
+        languages = {
+          nix.enable = true;
+          python.enable = true;
+          rust = {
+            enable = true;
+            crates.enable = true;
           };
-          viAlias = false;
-          vimAlias = true;
-          lsp = {
-             enable = true;
+          ts.enable = true;
+          lua.enable = true;
+          go.enable = true;
+          bash.enable = true;
+          #enableLSP = true;
+          enableTreesitter = true;
+          enableFormat = true;
+        };
+        viAlias = false;
+        vimAlias = true;
+        spellcheck = {
+          enable = true;
+          programmingWordlist.enable = true;
+        };
+
+        lsp = {
+          # This must be enabled for the language modules to hook into
+          # the LSP API.
+          enable = true;
+
+          formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = true;
+          lspSignature.enable = false; # conflicts with blink in maximal
+          otter-nvim.enable = true;
+          nvim-docs-view.enable = true;
+        };
+
+        debugger = {
+          nvim-dap = {
+            enable = true;
+            ui.enable = true;
           };
-          startPlugins = [
-             "harpoon"
-             "nvim-notify"
-             "render-markdown-nvim"
-          ];
-          statusline.lualine.enable = true;
-          telescope.enable = true;
-          autocomplete.nvim-cmp.enable = true;
-          terminal = { 
-             toggleterm = {
-                enable = true;
-                lazygit = {
-                   enable = true;
-                   direction = "float";
-                };
-             };
-             
+        };
+        startPlugins = [
+          "harpoon"
+          "nvim-notify"
+          "render-markdown-nvim"
+        ];
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        terminal = {
+          toggleterm = {
+            enable = true;
+            lazygit = {
+              enable = true;
+              direction = "float";
+            };
           };
-          lazy = {
-             enable = true;
+        };
+        lazy = {
+          enable = true;
+        };
+        assistant.copilot = {
+          enable = true;
+          cmp.enable = true;
+          setupOpts = {
+            panel = {
+              enable = true;
+              position = "right";
+            };
           };
-          assistant.copilot = {
-             enable = true;
-             cmp.enable = true;
-             setupOpts = {
-                panel = {
-                   enable = true;
-                   position = "right";
-                };
-             };
+        };
+        visuals = {
+          nvim-scrollbar.enable = true;
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          cinnamon-nvim.enable = true;
+          fidget-nvim.enable = true;
+
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+
+          # Fun
+          cellular-automaton.enable = false;
+        };
+
+        statusline = {
+          lualine = {
+            enable = true;
           };
-          #visuals.tiny-devicons-auto-colors.enable = true;
-          withNodeJs = true;
-          withPython3 = true;
-       }; 
+        };
+        #visuals.tiny-devicons-auto-colors.enable = true;
+        withNodeJs = true;
+        withPython3 = true;
+      };
     };
   };
   
