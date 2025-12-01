3308b5fe448f385e38f6976c88fb784e0ea863e5
Lukas Aurio <lukeaurio@proton.me>
2025-12-01 15:05:03 -0500

    feat(nvim): :sparkles: FINALLY! fixed the visuals so it doesn;t look so sad

--- nixos/etc/nixos/programming-configuration.nix
+++ nixos/etc/nixos/programming-configuration.nix
@@ -119,6 +119,28 @@ in {
             };
           };
         };
+        ui = {
+          borders.enable = true;
+          noice.enable = true;
+          colorizer.enable = true;
+          modes-nvim.enable = false; # the theme looks terrible with catppuccin
+          illuminate.enable = true;
+          breadcrumbs = {
+            enable = true;
+            navbuddy.enable = true;
+          };
+          smartcolumn = {
+            enable = true;
+            setupOpts.custom_colorcolumn = {
+              # this is a freeform module, it's `buftype = int;` for configuring column position
+              nix = "110";
+              ruby = "120";
+              java = "130";
+              go = ["90" "130"];
+            };
+          };
+          fastaction.enable = true;
+        };
         lazy = {
           enable = true;
         };
